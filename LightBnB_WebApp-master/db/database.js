6502cc7306849952491d68923aad5b3901992400
james <jameswu000@gmail.com>
2023-09-29 17:26:35 -0600

    refactor getAllProperties with form query fields and added sql query strings  with handler consts in db pool.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -131,19 +131,82 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-
+/////////////////////////////////////////////////////////
 const getAllProperties = function (options, limit = 10) {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
+  let queryParams = []
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
 
+  if (options.city) {
+    queryParams.push(`%${options.city}%`)
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id)
+
+    if (options.city) {
+      queryString += `AND properties.owner_id = $${queryParams.length}`
+    } else {
+      queryString += `WHERE properties.owner_id = $${queryParams.length}`
+    }
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100)
+
+    if (options.city || options.owner_id) {
+      queryString += `
+      AND properties.cost_per_night >= $${queryParams.length}
+      `
+    } else {
+      queryString += `
+      WHERE properties.cost_per_night >= $${queryParams.length}
+      `
+    }
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100)
+
+    if (options.city || options.owner_id || options.minimum_price_per_night) {
+      queryString += `
+      AND properties.cost_per_night <= $${queryParams.length}
+      `
+    } else {
+      queryString += `
+      WHERE properties.cost_per_night <= $${queryParams.length}
+      `
+    }
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating, limit)
+
+    queryString += `
+    GROUP BY properties.id 
+    HAVING AVG(property_reviews.rating) >= $${queryParams.length-1}
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length}
+    `
+  } else {
+    queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length}
+    `
+  }
+
+  return pool
+    .query(queryString, queryParams)
+    .then(res => res.rows)
+    .catch(err => console.log(err.message))
+
+  
+}};
+//////////////////////////////////////////////////////////////
 
 /**
  * Add a property to the database
